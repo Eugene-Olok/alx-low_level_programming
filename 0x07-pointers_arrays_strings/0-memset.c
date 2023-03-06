35064183112d8db3d698ab432c6b05a237ca015c
Eugene-Olok <eugeneolok@gmail.com>
2023-03-06 14:39:19 -0800

    a function that copies memory area.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,10 +13,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
 
-	for (index = 0; n > 0; index++)
+	for (; n > 0; index++)
 	{
 		s[index] = b;
-		n -= 1;
+		n--;
 	}
 	return (s);
 }
