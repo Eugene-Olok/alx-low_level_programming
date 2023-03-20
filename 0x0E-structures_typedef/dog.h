3320f6d108c78602a8e11f420e775cc074c53693
Eugene-Olok <eugeneolok@gmail.com>
2023-03-20 15:14:02 -0700

    a new type struct dog with the following elements:

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef _dog_h_
 #define _dog_h_
-
 /**
 * struct dog - such structure, wow
 * @name: dog's name
@@ -13,15 +12,11 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}
-dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
 
 #endif
